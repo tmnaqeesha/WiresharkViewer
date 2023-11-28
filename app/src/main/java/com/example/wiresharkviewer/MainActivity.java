d3d3f97860925b309fd8c1e757126b81e4232cb0
tmnaqeesha <tmnaqeesha@users.noreply.github.com>
2023-11-28 23:31:49 +0100

    Initial Commit

---
+++ app/src/main/java/com/example/wiresharkviewer/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.wiresharkviewer;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+public class MainActivity extends AppCompatActivity {
+    private WebView mywebview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mywebview = (WebView) findViewById(R.id.webView);
+        WebSettings websettings = mywebview.getSettings();;
+        websettings.setJavaScriptEnabled(true);
+        mywebview.loadUrl("https://login.univ-cotedazur.fr/login?service=https://ent.univ-cotedazur.fr/uPortal/Login");
+        mywebview.setWebViewClient(new WebViewClient());
+    }
+    public void onBackPresses() {
+        if(mywebview.canGoBack()) {
+            mywebview.goBack();
+        }
+        else {
+            super.onBackPressed();
+        }
+    }
+}
