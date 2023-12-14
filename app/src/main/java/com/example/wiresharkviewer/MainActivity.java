e40b60c559f34b4a1ec6aefffbeb4e81f46d53d4
tmnaqeesha <tmnaqeesha@users.noreply.github.com>
2023-12-14 11:56:02 +0100

    Table example

--- app/src/main/java/com/example/wiresharkviewer/MainActivity.java
+++ app/src/main/java/com/example/wiresharkviewer/MainActivity.java
@@ -1,14 +1,46 @@
 package com.example.wiresharkviewer;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
-public class MainActivity extends AppCompatActivity {
+import androidx.cardview.widget.CardView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView card1, card2, card3, card4 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        card1 = (CardView) findViewById(R.id.card_tous);
+        card2 = (CardView) findViewById(R.id.card_tcp);
+        card3 = (CardView) findViewById(R.id.card_ipv4);
+        card4 = (CardView) findViewById(R.id.card_arp);
+
+        card1.setOnClickListener((View.OnClickListener) this);
+        card2.setOnClickListener((View.OnClickListener) this);
+        card3.setOnClickListener((View.OnClickListener) this);
+        card4.setOnClickListener((View.OnClickListener) this);
+
+
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        if (v.getId() == R.id.card_tous) {
+            intent = new Intent(this, Tous.class);
+            startActivity(intent);
+        } else if (v.getId() == R.id.card_tcp) {
+            // Handle card_tcp click
+        } else if (v.getId() == R.id.card_ipv4) {
+            // Handle card_ipv4 click
+        } else if (v.getId() == R.id.card_arp) {
+            // Handle card_arp click
+        }
+    }
 }
-// testing github
+
---
+++ app/src/main/java/com/example/wiresharkviewer/Tous.java
@@ -0,0 +1,58 @@
+package com.example.wiresharkviewer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Tous extends AppCompatActivity {
+
+    ListView lstStockItems;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_tous);
+        lstStockItems = findViewById(R.id.lstStockItems);
+
+        FillList();
+    }
+
+    public void FillList() {
+        try {
+
+            Items items = new Items();
+            List<Map<String, String>> prolist = new ArrayList<Map<String, String>>();
+            String[] from = {"ItemRef", "Item", "Supplier", "LatestStock", "SalePrice", "BinNumber", "Balance"};
+            int[] views = {R.id.txtItemRef, R.id.txtItemName, R.id.txtSupplier,
+                    R.id.txtLatestStock, R.id.txtSalePrice, R.id.txtBinNumber, R.id.txtBalance};
+
+            List<Items> lsr = items.GetItems();
+
+            for (int i = 0; i < lsr.size(); i++) {
+                Map<String, String> datanum = new HashMap<String, String>();
+                datanum.put("ItemRef", lsr.get(i).ItemRef);
+                datanum.put("Item", String.valueOf(lsr.get(i).Item));
+                datanum.put("Supplier", String.valueOf(lsr.get(i).Supplier));
+                datanum.put("LatestStock", String.valueOf(lsr.get(i).LatestStock));
+                datanum.put("SalePrice", String.valueOf(lsr.get(i).SalePrice));
+                datanum.put("BinNumber", String.valueOf(lsr.get(i).BinNumber));
+                datanum.put("Balance", String.valueOf(lsr.get(i).Balance));
+                prolist.add(datanum);
+            }
+
+            final SimpleAdapter simpleAdapter = new SimpleAdapter(Tous.this,
+                    prolist, R.layout.table, from,
+                    views);
+            lstStockItems.setAdapter(simpleAdapter);
+        } catch (Exception ex) {
+            Toast.makeText(Tous.this, ex.getMessage().toString(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+}
